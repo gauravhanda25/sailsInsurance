51dd3305c4190a0dde7704522e4998e0e8127399
Gaurav Handa <gaurav@aroracomfortechs.com>
2017-12-09 17:08:21 +0530

    update plan code

--- api/controllers/PlanController.js
+++ api/controllers/PlanController.js
@@ -36,9 +36,29 @@ module.exports = {
    * `PlanController.updatePlan()`
    */
   updatePlan: function (req, res) {
-    return res.json({
-      todo: 'updatePlan() is not implemented yet!'
-    });
+    Plan.findOne({id:req.body.id}).exec(function(error, plan) {
+    if(error) {
+        // do something with the error.
+    }
+    console.log(plan, req.body.id);
+    if(req.body.email) {
+        // validate whether the email address is valid?
+
+        // Then save it to the object.
+        plan.cover = req.body.cover;
+    }
+    // Repeat for each eligible attribute, etc.
+    if(plan) {
+      plan.save(function(error, updatedPlan) {
+          if(error) {
+              // do something with the error.
+          } else {
+              // value saved!
+              res.json(updatedPlan);
+          }
+      });
+    }
+  });
   },
 
 
